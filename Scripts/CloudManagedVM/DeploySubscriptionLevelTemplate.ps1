7eefaffe3fb02f962c7b6708b419585165c830c0
raviprpandey <69055793+raviprpandey@users.noreply.github.com>
2021-04-21 17:44:59 -0700

    Added support for subscription level templates as well
    * Added support for subscription level deployment
    
    * Renamed CloudTemplates to SubscriptionLevelTemplates
    
    * Provide defaultvalue for image/vm deployment names
    
    * Removed the temaplate param values
    
    * Cosmetic changes

---
+++ Scripts/CloudManagedVM/DeploySubscriptionLevelTemplate.ps1
@@ -0,0 +1,69 @@
+﻿[CmdletBinding()]
+Param(
+    [Parameter(Mandatory=$True)]
+    [ValidateNotNullOrEmpty()]
+    # Resource group containing azure device
+    [string]$saasResourceGroup,
+
+    [Parameter(Mandatory=$True)]
+    [ValidateNotNullOrEmpty()]
+    # Azure ASE device name
+    [string]$deviceName,
+
+    [Parameter(Mandatory=$True)]
+    [ValidateNotNullOrEmpty()]
+    # Azure subscription id
+    [string]$subscriptionId,
+
+    [Parameter(Mandatory=$True)]
+    [ValidateNotNullOrEmpty()]
+    # Access key for azure subscription access
+    # Please refer to https://docs.microsoft.com/en-us/azure/databox-online/azure-stack-edge-gpu-deploy-virtual-machine-cli-python on how to obtain
+    # tenantId and clientId
+    [string]$saasAccessKey,
+
+    [Parameter(Mandatory=$True)]
+    [ValidateNotNullOrEmpty()]
+    # Tenant id
+    [string]$tenantId,
+
+    [Parameter(Mandatory=$True)]
+    [ValidateNotNullOrEmpty()]
+    # Client id
+    [string]$clientId,
+
+    [Parameter(Mandatory=$True)]
+    [ValidateNotNullOrEmpty()]
+    # Template file path
+    [string]$templateFilePath,
+
+    [Parameter(Mandatory=$True)]
+    [ValidateNotNullOrEmpty()]
+    # Template Params file path
+    [string]$templateParamsFilePath,
+
+    [Parameter(Mandatory=$False)]
+    [ValidateNotNullOrEmpty()]
+    # Cloud Managed VM dll and its dependencies path
+    [string]$dllPath = ".\",
+
+    [Parameter(Mandatory=$True)]
+    [ValidateNotNullOrEmpty()]
+    # Location
+    [string]$location = "dbelocal"
+)
+
+function LoadDlls($dllPath)
+{
+    $dllFiles = ls $dllPath*.dll
+    foreach ($f in $dllFiles)
+    {
+        [Reflection.Assembly]::LoadFile($f.FullName)
+    }
+}
+
+
+LoadDlls $dllPath
+$client = New-Object CloudManagedVM.CloudLib($saasAccessKey, $tenantId, $clientId, $subscriptionId)
+
+$client.DeployTemplateAtSubscriptionLevel($saasResourceGroup, $deviceName, $templateFilePath, $templateParamsFilePath, $location)
---
+++ dotnetSamples/CloudManaged/CloudManagedVM/CloudManagedVM/SampleDeployments.cs
@@ -0,0 +1,18 @@
+﻿
+namespace CloudManagedVM
+{
+    class SampleDeployments
+    {
+        public static void DeploySubscriptionLevelTemplate(string accessToken, string subscriptionId, string deviceName, string saaResourceGroup, string templateFilePath, string templateParamsFilePath, string location = "dbelocal")
+        {
+            var client = new CloudLib(accessToken, subscriptionId);
+            client.DeployTemplateAtSubscriptionLevel(saaResourceGroup, deviceName, templateFilePath, templateParamsFilePath, location);
+        }
+
+        public static void DeployResourceGroupLevelTemplate(string accessToken, string subscriptionId, string deviceName, string saaResourceGroup, string templateFilePath, string templateParamsFilePath, string linkedResourceGroup)
+        {
+            var client = new CloudLib(accessToken, subscriptionId);
+            client.DeployTemplateAtResourceGroupLevel(saaResourceGroup, deviceName, templateFilePath, templateParamsFilePath, linkedResourceGroup);
+        }
+    }
+}
